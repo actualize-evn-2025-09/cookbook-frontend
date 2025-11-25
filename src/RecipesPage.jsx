c89681b468fb46e63700510d31be61499c7f31c8
leonshimizu <lmshimizu@gmail.com>
2025-11-25 10:16:30 +1000

    Make the handleIndex function to make the frontend dynamic and make requests to the backend

--- src/RecipesPage.jsx
+++ src/RecipesPage.jsx
@@ -1,25 +1,34 @@
 import { RecipesIndex } from "./RecipesIndex";
 import { RecipesNew } from "./RecipesNew";
 import axios from "axios";
+import { useState, useEffect } from 'react';
 
 export function RecipesPage() {
   // where all the data comes from
   // prop - how we get data from one component to the next
   // we'll be using a 3rd party library called axios to make requests to our backend
   // react state 
-  let recipes = [];
+  // useState - react hook
+  const [recipes, setRecipes] = useState([]);
+  // special state variable - recipes
+  // function to update the state variable - setRecipes
 
   const handleIndex = () => {
     axios.get("http://localhost:3000/recipes.json").then((response) => {
       console.log(response.data);
-      recipes = response.data;
+      setRecipes(response.data);
     })
   }
+
+  // handleIndex();
+  useEffect(handleIndex, []);
+  // useEffect - react hook
+  // first argument - what we want it to do
+  // second argument - when we want it to run - [] means on page load
 
   return (
     <div>
       <RecipesNew />
-      <button onClick={handleIndex}>Load Recipes</button>
       <RecipesIndex recipesProp={recipes} />
     </div>
   );
